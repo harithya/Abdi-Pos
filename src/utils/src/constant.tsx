2462cb89dbd591a11268cda26229615258e739cf
Harithya Wisesa <43434776+harithya@users.noreply.github.com>
2022-06-16 10:15:34 +0700

    menambahkan validasi pada tamu harus lunas

--- src/utils/src/constant.tsx
+++ src/utils/src/constant.tsx
@@ -1,10 +1,13 @@
-
-
 const constant = {
     container: 24,
     statusBarPadding: 30,
+    // baseUrl: "http://192.168.0.116:8000/api",
+    // mainUrl: "http://192.168.0.116:8000",
+
     baseUrl: "https://health.indika.id/api",
-    mainUrl: "https://health.indika.id",
+    mainUrl: "https://health.indika.id/api",
+
+
 
 
     transactionSuccess: 1,
