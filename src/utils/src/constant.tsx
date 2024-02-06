e61a74a7ac1ac90d897626f0549e75a6b42e453f
Harithya Wisesa <harithya77@gmail.com>
2024-02-06 18:14:11 +0700

    add ts ignore to helper

--- src/utils/src/constant.tsx
+++ src/utils/src/constant.tsx
@@ -1,20 +1,16 @@
 const constant = {
-    container: 24,
-    statusBarPadding: 30,
-    // baseUrl: "http://192.168.0.169:8000/api",
-    // mainUrl: "http://192.168.0.169:8000",
+  container: 24,
+  statusBarPadding: 30,
+  // baseUrl: 'https://clinic.cloudapps.id/api',
+  baseUrl: 'https://192.168.0.100:8000/api',
+  mainUrl: 'https://192.168.0.100:8000',
 
-    baseUrl: "https://arfan-medika.cloudapps.id/api",
-    mainUrl: "https://arfan-medika.cloudapps.id",
+  // baseUrl: "https://arfan-medika.cloudapps.id/api",
+  // mainUrl: "https://arfan-medika.cloudapps.id",
 
-
-
-
-
-    transactionSuccess: 1,
-    transactionCancel: 2,
-    transactionReturn: 3,
-
-}
+  transactionSuccess: 1,
+  transactionCancel: 2,
+  transactionReturn: 3,
+};
 
 export default constant;
